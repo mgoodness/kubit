ebeee3918e10c49b2a5096a1afd4775f79b0f18f
icereval <icereval@gmail.com>
2017-01-22 20:26:08 -0500

    use cluster specific route table

--- modules/vpc/public_subnets.tf
+++ modules/vpc/public_subnets.tf
@@ -27,15 +27,21 @@ resource "aws_internet_gateway" "gateway" {
   }
 }
 
-resource "aws_route" "public" {
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.gateway.id}"
-  route_table_id = "${aws_vpc.main.main_route_table_id}"
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gateway.id}"
+  }
+  tags {
+    KubernetesCluster = "${var.cluster_name}"
+    Name = "${var.cluster_name}-public"
+  }
 }
 
 resource "aws_route_table_association" "public" {
   count = "${length(var.subnets["public_cidr_blocks"])}"
-  route_table_id = "${aws_vpc.main.main_route_table_id}"
+  route_table_id = "${aws_route_table.public.id}"
   subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
 }
 
